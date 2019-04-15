eecf4066fb6164e3a999aaa3d0b3696f130f32a3
Eli Wemyss <eliwemyss@gmail.com>
2019-04-15 15:28:14 -0700

    added favicon

--- src/info/aboutMe.js
+++ src/info/aboutMe.js
@@ -1,7 +1,7 @@
 const aboutMe = {
     tagline: "Hi, my name is Eli Wemyss and I'm a FullStack Web Developer",
     description: [
-        "I have experience with modern front-end and back-end JavaScript frameworks. My interest in coding started in high school, when I would get bored during study hall and would write programs on my TI-84 calculator, but only recently have I decided to make a career switch.",
+        "Originally from outside of Nashville, Tennessee, I'm currently based out of Portland, Oregon.I have experience with modern front-end and back-end JavaScript frameworks. My interest in coding started in high school, when I would get bored during study hall and would write programs on my TI-84 calculator, but only recently have I decided to make a career switch.",
         "When not coding, I enjoy skiing, cycling, and drinking craft beer."
     ],
 }
