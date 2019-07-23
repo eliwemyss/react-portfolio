de312ecca226a887b48f03479eaaa18eb4561d60
Eli Wemyss <eliwemyss@gmail.com>
2019-07-23 11:26:39 -0700

    fixed typo

--- src/info/aboutMe.js
+++ src/info/aboutMe.js
@@ -1,7 +1,7 @@
 const aboutMe = {
     tagline: "Hi, my name is Eli Wemyss and I'm a FullStack Web Developer",
     description: [
-        "I am currently relocating to Nashvill, TN, and I have experience with modern front-end and back-end JavaScript frameworks. My interest in coding started in high school, when I would get bored during study hall and would write programs on my TI-84 calculator, but only recently have I decided to make a career switch.",
+        "I am currently relocating to Nashville, TN, and I have experience with modern front-end and back-end JavaScript frameworks. My interest in coding started in high school, when I would get bored during study hall and would write programs on my TI-84 calculator, but only recently have I decided to make a career switch.",
         "When not coding, I enjoy skiing, cycling, and drinking craft beer."
     ],
 }
