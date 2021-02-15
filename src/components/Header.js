4fe62b70a5b6835e9895cf886761c3cbeba7580b
eliwemyss <eliwemyss@gmail.com>
2021-02-15 11:33:32 -0600

    added blog to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,9 +12,13 @@ const Header = () => {
                 <Link to="scrollProjects" smooth={true} duration={500} offset={-25} tabIndex={0}>
                     <span className="navLink">PROJECTS</span>
                 </Link>
+                     <Link to="" smooth={true} duration={500} tabIndex={0}>
+                    <span className="navLink">BLOG</span>
+                </Link>
                 <Link to="scrollContact" smooth={true} duration={500} tabIndex={0}>
                     <span className="navLink">CONTACT</span>
                 </Link>
+             
             </nav>
         </header>
     );
