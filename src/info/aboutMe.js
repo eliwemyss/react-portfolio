13f379cd3f0782046f7355a9296f1e0532c8eef9
eliwemyss <ewemyss@Administrators-MacBook-Air.local>
2021-02-15 11:28:39 -0600

    updated bio

--- src/info/aboutMe.js
+++ src/info/aboutMe.js
@@ -1,8 +1,8 @@
 const aboutMe = {
     tagline: "Hi, my name is Eli Wemyss and I'm a FullStack Web Developer",
     description: [
-        "I am currently relocating to Nashville, TN, and I have experience with modern front-end and back-end JavaScript frameworks. My interest in coding started in high school, when I would get bored during study hall and would write programs on my TI-84 calculator, but only recently have I decided to make a career switch.",
-        "When not coding, I enjoy skiing, cycling, and drinking craft beer."
+        "I am currently located in Nashville, TN, and I have experience with modern front-end and back-end JavaScript frameworks. My interest in coding started in high school, when I would get bored during study hall and would write programs on my TI-84 calculator, but only recently have I decided to make a career switch.",
+        "When not coding, I enjoy skiing, backpacking/hiking, cycling, and drinking craft beer."
     ],
 }
 
